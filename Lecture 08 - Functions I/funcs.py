b47b61e92e6666bdac7ec456192229615b49e1df
IrakliTabatadze <irakli.tabatadze.80@gmail.com>
2026-05-06 22:01:15 +0400

    Lecture 8

--- Lecture 08 - Functions I/funcs.py
+++ Lecture 08 - Functions I/funcs.py
@@ -1,151 +1,151 @@
-# print('Hello')
-#
-# import time
-# print(time.time())
+print('Hello')
+
+import time
+print(time.time())
 
 
-# from random import randint
+from random import randint
 
 
-# def calculate_sum():
-#     """Calculate the sum of a number"""
-#
-#     a = 5
-#     b = 10
-#     print(a + b)
-#
-#
-# calculate_sum()
-# calculate_sum()
-# calculate_sum()
+def calculate_sum():
+    """Calculate the sum of a number"""
 
-# def CalculateSum():
-#     """Calculate the sum of a number"""
-#
-#     a = 5
-#     b = 10
-#     print(a + b)
-# CalculateSum()
+    a = 5
+    b = 10
+    print(a + b)
 
 
-# def add(a, b, *args):
-#     print(a + b)
-#
-#
-# add(10, 15, 15, 15, 15, 15)
-# add(20, 25)
+calculate_sum()
+calculate_sum()
+calculate_sum()
+
+def CalculateSum():
+    """Calculate the sum of a number"""
+
+    a = 5
+    b = 10
+    print(a + b)
+CalculateSum()
 
 
-# def test_func(*args):
-#     print(args)
-#
-#
-# test_func(1, 2, 3, 4, 4)
+def add(a, b, *args):
+    print(a + b)
 
 
-# def add(*args, a, b):
-    # print(a + b)
-    # print(args)
-    # print(a)
-    # print(b)
+add(10, 15, 15, 15, 15, 15)
+add(20, 25)
 
-#     total = 0
-#
-#     for item in args:
-#         total += item
-#
-#
-#
-#
-# add(10, 15, 15, 15, 15, 15, b=125, a=45)
-# add(20, 25)
+
+def test_func(*args):
+    print(args)
+
+
+test_func(1, 2, 3, 4, 4)
+
+
+def add(*args, a, b):
+    print(a + b)
+    print(args)
+    print(a)
+    print(b)
+
+    total = 0
+
+    for item in args:
+        total += item
 
 
 
 
-# def full_name(first_name, last_name='Smith', *args, **kwargs):
-#     print(f'{first_name} {last_name}, {args}, {kwargs}')
-
-
-# name = input('What is your name? ')
-# lname = input('What is your last name? ')
-# other_info = input('What is your other info? ')
-# profession = input('What is your profession? ')
-# hobby = input('What is your hobby? ')
-
-# full_name(name, lname, profession=profession, hobby=hobby)
+add(10, 15, 15, 15, 15, 15, b=125, a=45)
+add(20, 25)
 
 
 
 
-# def append_item(item, lst=[]):
-#     lst.append(item)
-#     print(lst)
+def full_name(first_name, last_name='Smith', *args, **kwargs):
+    print(f'{first_name} {last_name}, {args}, {kwargs}')
 
 
-# items_list = ['banana', 'pineapple']
-# append_item('apple', items_list)
+name = input('What is your name? ')
+lname = input('What is your last name? ')
+other_info = input('What is your other info? ')
+profession = input('What is your profession? ')
+hobby = input('What is your hobby? ')
 
-# append_item('apple')
-# append_item('banana')
-
-
-
-
-# def append_item(item, lst=None):
-    # if lst is None:
-    #     lst = []
-    # if not lst:
-    #     lst = []
-
-    # lst.append(item)
-    # print(lst)
-
-
-# items_list = ['banana', 'pineapple']
-#
-# append_item('apple', items_list)
-
-# append_item('apple')
-# append_item('banana')
+full_name(name, lname, profession=profession, hobby=hobby)
 
 
 
 
-# def full_name(first_name, last_name):
-#     f_name = f'{first_name} {last_name}'
-#
-#     return f_name
-#
-# def greetings(full_name):
-#     return f'Hello {full_name}'
-#
-#
-# fname = input('Enter first name: ')
-# lname = input('Enter last name: ')
-#
-#
-# f_name = full_name(fname, lname)
-# greeting_txt = greetings(f_name)
-#
-# print(greeting_txt)
+def append_item(item, lst=[]):
+    lst.append(item)
+    print(lst)
+
+
+items_list = ['banana', 'pineapple']
+append_item('apple', items_list)
+
+append_item('apple')
+append_item('banana')
 
 
 
-# def check_age(age):
-#     if age < 0:
-#         return
-#
-#     print('after first if')
-#     if age < 18:
-#         return 'Age is younger than 18'
-#     else:
-#         return 'Age is older than 18'
-#
-#     print('after second if')
 
-# check = check_age(20)
-# print(check)
+def append_item(item, lst=None):
+    if lst is None:
+        lst = []
+    if not lst:
+        lst = []
+
+    lst.append(item)
+    print(lst)
+
+
+items_list = ['banana', 'pineapple']
+
+append_item('apple', items_list)
+
+append_item('apple')
+append_item('banana')
+
+
+
+
+def full_name(first_name, last_name):
+    f_name = f'{first_name} {last_name}'
+
+    return f_name
+
+def greetings(full_name):
+    return f'Hello {full_name}'
+
+
+fname = input('Enter first name: ')
+lname = input('Enter last name: ')
+
+
+f_name = full_name(fname, lname)
+greeting_txt = greetings(f_name)
+
+print(greeting_txt)
+
+
+
+def check_age(age):
+    if age < 0:
+        return
+
+    print('after first if')
+    if age < 18:
+        return 'Age is younger than 18'
+    else:
+        return 'Age is older than 18'
+
+    print('after second if')
+
+check = check_age(20)
+print(check)
 
 
 
